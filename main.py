0e8376936909153b1d33d65c362f50140dd5ae44
Allen Zhao <60819887+Jamsterdam@users.noreply.github.com>
2021-05-26 08:50:27 +0800

    Update main.py
    write the working flow of the main.py

--- main.py
+++ main.py
@@ -3,11 +3,20 @@
 import pandas as pd
 
 
-# read the csv file, but I need to read it no matter what nane it has,
+# read the csv file, but I need to read it no matter what name it has,
 # for example, pd.read_csv('*.csv')
 notion = pd.read_csv('Autol CRM a376400f34a2458797bd2abba4fd49e7.csv')
 
+# write a loop to traverse the each cell needs to be modified.
 
-# read the related oem column
-# process each row with the text_handle.py, and get replace the original cell of the spreadsheet
-# export as xlsx file
+  # read only the specific cell in the file.
+
+  # pass the cell data to the text_handle.py
+
+  # read the result of the text_handle.py, 
+
+  # assign the result to a variable
+
+  # write the variable to the specific cell.
+
+# export the modified csv as xlsx file
